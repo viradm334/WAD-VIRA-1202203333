be27836fcb01b11c0bccbf19ef21e2e2b1603b61
viradm334 <viradamayanti@student.telkomuniversity.ac.id>
2022-11-23 07:49:51 +0700

    debug

--- MODUL3 VIRA/pages/config/delete.php
+++ MODUL3 VIRA/pages/config/delete.php
@@ -13,7 +13,7 @@ if(isset($_GET['op'])){
 
 if($op == 'delete'){
     $id = $_GET['id'];
-    $sql1 = "delet from mobil where id_mobil= '$id'";
+    $sql1 = "delete from mobil where id_mobil= '$id'";
     $q1 = mysqli_query($koneksi, $sql1);
     if($q1){
         $sukses = 'Berhasil menghapus data';
